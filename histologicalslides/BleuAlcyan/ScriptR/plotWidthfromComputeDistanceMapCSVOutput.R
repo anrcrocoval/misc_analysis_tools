b876ec3eda251747f8a5218b18415d05594d4b29
Perrine Paul-Gilloteaux <perrine.paul-gilloteaux@univ-nantes.fr>
2022-03-03 10:25:30 +0100

    New presentation of results

--- histologicalslides/BleuAlcyan/ScriptR/plotWidthfromComputeDistanceMapCSVOutput.R
+++ histologicalslides/BleuAlcyan/ScriptR/plotWidthfromComputeDistanceMapCSVOutput.R
@@ -2,7 +2,9 @@ setwd("C:/Users/paul-gilloteaux-p/Documents/GitHub/ANRCROCOVAL/misc_analysis_too
 #file.choose()
 # Install the released version from CRAN:
 #install.packages("stringr")
+#install.packages("zoo")
 library("stringr")
+library("zoo") #maybe to use rollmean
 listfile=dir()
 
 i=0
@@ -15,21 +17,28 @@ for (n in listfile)
 	{
 		print(n)
 		datW <-read.csv(n, sep=",")
-		plot(datW$storenormalizedpos[-1],datW$storewidthum[-1],type="b",pch=20, col =2) #plot all but first element
+		plot(datW$storenormalizedpos[-1],datW$storewidthum[-1],type="b",pch=20, col =2,ylim=c(0,350)) #plot all but first element
+		
 		par(new=TRUE)
+
 		n=str_replace(n, "Absolutewidth.csv", "AbsoluteBA.csv")
 		print(n)
 		datBA <-read.csv(n, sep=",")
-		plot(datBA$storenormalizedposBA[-1],datBA$storeBApc[-1]*100,type="b",,pch=20,col=3, axes = FALSE, xlab="",ylab="") #plot all but first element
-		axis(side=4,at=pretty(range(datBA$storeBApc[-1]*100)))		
-		mtext("pc covered by BA on geodesic lines", side =4,line=3)
+		plot(datBA$storenormalizedposBA[-1],datBA$storeBApc[-1]*datW$storewidthum[-1],type="b",pch=20,col=3, ylim=c(0,350),axes = FALSE, xlab="",ylab="") #plot all but first element
+		title(str_replace(n, "AbsoluteBA.csv",""))
+	
+		readline(prompt="Press [enter] to continue")
+		readline(prompt="Press [enter] to continue")
+		par(new=FALSE)
+		plot(rollmean(datBA$storeBApc[-1]*100,10),type="b",pch=20,col=3, ylim=c(0,100)) #plot all but first element
 		title(str_replace(n, "AbsoluteBA.csv",""))
 	
 		readline(prompt="Press [enter] to continue")
 		readline(prompt="Press [enter] to continue")
 
-
 	}
+
+
 
 
 }
